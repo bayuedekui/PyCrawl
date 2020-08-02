8bedfa939d037e7f776b71c9cfa7edfb27fcafbd
bayuedekui <468360786@qq.com>
2020-08-02 10:45:31 +0800

    完全实现抽取17自学吧功能

--- 17zixueba.com/extral_detail.py
+++ 17zixueba.com/extral_detail.py
@@ -1,8 +1,12 @@
 import time
+import sys
+import pymysql
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import xlwt
 import xlrd
+from openpyxl import Workbook
+import sys
 
 driver = webdriver.Chrome()
 driver.maximize_window()
@@ -10,91 +14,63 @@ driver.get('https://www.17zixueba.com')
 
 time.sleep(30)
 
-# 构造存入的excel
-video_url = xlwt.Workbook(encoding='utf-8', style_compression=0)
-video_sheet = video_url.add_sheet('17自学把', cell_overwrite_ok=True)
-video_sheet.write(0, 0, '视频名称')
-video_sheet.write(0, 1, '视频详情链接')
-video_sheet.write(0, 2, '视频访问量')
-video_sheet.write(0, 3, '视频评论量')
-video_sheet.write(0, 4, '视频发布时间')
-video_sheet.write(0, 5, '视频类型')
-video_sheet.write(0, 6, '视频详情')
-video_sheet.write(0, 7, '视频下载链接密码')
-n = 1
+# 从数据库中读取数据
+con = pymysql.connect(host='127.0.0.1', user='root', password='123456', database='video_crawl', charset='utf8')
+cursor = con.cursor()
+cursor.execute('select * from video_info')
+data = cursor.fetchall()
 
-# 读取excel
-data = xlrd.open_workbook('17自学吧.xls')
-table = data.sheet_by_index(0)
-rows = table.nrows
-cols = table.ncols
+print(data)
+# 操作数据库的数据
+for i in range(len(data)):
+    r = data[i]
+    id = r[0]
+    url = r[2]
+    is_crawl = r[7]
+    if is_crawl is not None:
+        print("该链接已经爬取。。。")
+        continue
+    else:
+        print("【{}】准备id:{}提取：{}".format(i, id, url))
+        new_tab_js = "window.open('" + url + "')"
+        driver.execute_script(new_tab_js)
+        all_handles = driver.window_handles
+        driver.switch_to_window(all_handles[1])
+        try:
+            time.sleep(10)
+            detail_fonts = driver.find_element_by_xpath(
+                '//*[starts-with(@id,"postmessage_")]').find_elements_by_tag_name(
+                'font')
+            detail_text = []
+            for index, f in enumerate(detail_fonts):
+                if index % 2 == 1:
+                    font_text = f.text
+                    detail_text.append(font_text)
 
-excel_list = []
-for row in range(rows):
-    if row > 0:
-        row_value = table.row_values(row)
-        excel_list.append(row_value)
-        print(row_value[1])
+            video_detail = " ".join(detail_text)
+            link_psw_obj = driver.find_elements_by_class_name('showhide')
+            if len(link_psw_obj) == 2:
+                link_psw = link_psw_obj[1].text
+            elif len(link_psw_obj) == 1:
+                link_psw = link_psw_obj[0].text
+            else:
+                link_psw = "该视频未回复"
 
-# print(excel_list)
-# 操作表格的数据
-for i in range(len(excel_list)):
-    r = excel_list[i]
-    url = r[1]
-    # url='https://www.17zixueba.com/forum.php?mod=viewthread&tid=1214&highlight=python'
-    print("准备提取：{}".format(url))
-    new_tab_js = "window.open('" + url + "')"
-    driver.execute_script(new_tab_js)
-    all_handles = driver.window_handles
-    driver.switch_to_window(all_handles[1])
-    try:
-        time.sleep(5)
-        detail_fonts = driver.find_element_by_xpath('//*[starts-with(@id,"postmessage_")]').find_elements_by_tag_name('font')
-        detail_text = []
-        for index, f in enumerate(detail_fonts):
-            if index % 2 == 1:
-                font_text = f.text
-                detail_text.append(font_text)
+            print(video_detail + " | " + link_psw)
 
-        video_detail = " ".join(detail_text)
-        link_psw_obj = driver.find_elements_by_class_name('showhide')
-        if len(link_psw_obj)==2:
-            link_psw=link_psw_obj[1].text
-        else:
-            link_psw=link_psw_obj[0].text
+            update_sql = "update video_info set video_detail='" + str(video_detail) + "', video_link_psw='" + str(link_psw) + "' where id='" + str(id) + "'"
+            print("update_sql is:{}".format(update_sql))
+            cursor.execute(update_sql)
+            con.commit()
 
+            driver.close()
+            driver.switch_to_window(all_handles[0])
+        except NoSuchElementException as e:
+            print("提取出现异常:{}".format(e))
 
-        print(video_detail + " | " + link_psw)
+            driver.close()
+            driver.switch_to_window(all_handles[0])
 
-        video_sheet.write(n, 0, r[0])
-        video_sheet.write(n, 1, r[1])
-        video_sheet.write(n, 2, r[2])
-        video_sheet.write(n, 3, r[3])
-        video_sheet.write(n, 4, r[4])
-        video_sheet.write(n, 5, r[5])
-        video_sheet.write(n, 6, video_detail)
-        video_sheet.write(n, 7, link_psw)
-        n = n + 1
-
-        driver.close()
-        driver.switch_to_window(all_handles[0])
-    except NoSuchElementException as e:
-        print("提取出现异常:{}".format(e))
-        video_sheet.write(n, 0, r[0])
-        video_sheet.write(n, 1, r[1])
-        video_sheet.write(n, 2, r[2])
-        video_sheet.write(n, 3, r[3])
-        video_sheet.write(n, 4, r[4])
-        video_sheet.write(n, 5, r[5])
-        video_sheet.write(n, 6, "提取异常")
-        video_sheet.write(n, 7, "提取异常")
-        n = n + 1
-        driver.close()
-        driver.switch_to_window(all_handles[0])
-
-
-    print(i)
     # if i == 5:
     #     break
 
-video_url.save('17自学吧detail.xls')
--- insert_mysql.py
+++ insert_mysql.py
@@ -1,40 +1,38 @@
 import pymysql
 import xlrd
 
-name_list = ['web前端.xls', 'java视频教程.xls', 'python视频教程.xls', '微信开发视频教程.xls', '大数据云计算.xls']
+name_list = ['17自学吧.xls']
 
 data_file = []
 
 
 # 提取数据入库
-# def extract_data(file_name):
-#     table = xlrd.open_workbook(file_name).sheet_by_index(0)
-#     row = table.nrows
-#     for r in range(row):
-#         if r > 0:
-#             data_file.append(table.row_values(r))
+def extract_data(file_name):
+    table = xlrd.open_workbook(file_name).sheet_by_index(0)
+    row = table.nrows
+    for r in range(row):
+        if r > 0:
+            data_file.append(table.row_values(r))
 
 
-# if __name__ == '__main__':
-#     for name in name_list:
-#         extract_data(name)
-#     print(data_file)
+for name in name_list:
+    extract_data(name)
+print(data_file)
 
 conn = pymysql.connect(host='127.0.0.1', user='root', password='123456', database='video_crawl', charset='utf8')
 
-# for row in data_file:
-#     cur = conn.cursor()
-#     sql = "insert into video_info(video_name,video_url,video_detail,video_visit,video_reply,video_release,video_type) values('{}','{}','{}','{}','{}','{}','{}')".format(
-#         row[0], row[1], '', row[2], row[3], row[4], row[5])
-#     cur.execute(sql)
-#     cur.close()
+for row in data_file:
+    cur = conn.cursor()
+    sql = "insert into video_info(video_name,video_url,video_visit,video_reply,video_release,video_type) values('{}','{}','{}','{}','{}','{}')".format(
+        row[0], row[1], row[2], row[3], row[4], row[5])
+    cur.execute(sql)
+    cur.close()
 
-cur=conn.cursor()
-sql='select * from video_info'
-cur.execute(sql)
-res=list(cur.fetchall())
-print(res[0][2])
-
+# cur = conn.cursor()
+# sql = 'select * from video_info'
+# cur.execute(sql)
+# res = list(cur.fetchall())
+# print(res[0][2])
 
 # conn.commit()
 conn.close()
